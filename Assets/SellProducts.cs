ff6021241bd15073471d15139900388c22bb8a8f
yo-zh <sasha7onoff@gmail.com>
2023-10-20 16:15:02 +0300

    Delete old and unused code from SellProduct script

--- Assets/SellProducts.cs
+++ Assets/SellProducts.cs
@@ -4,39 +4,7 @@ using UnityEngine;
 
 public class SellProducts : MonoBehaviour
 {
-    public List<string> orderList = new();
-    /*
-
-    [SerializeField] GameObject customer = null;
-    [SerializeField] GameObject[] orderArray;
-    [SerializeField] List<string> orderList;
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (GameObject.FindGameObjectsWithTag("CurrentCustomer") != null && customer == null)
-        {
-            customer = GameObject.FindGameObjectWithTag("CurrentCustomer");
-            orderArray = customer.GetComponent<CustomerOrder>().orderArray;
-            foreach (GameObject order in orderArray)
-            {
-                orderList.Add(order.gameObject.name);
-            }
-        }
-        else if (customer != null)
-        {
-            if (orderList.Contains(other.gameObject.name))
-            {
-                orderList.Remove(other.gameObject.name);
-                Destroy(other.gameObject);
-            }
-            if (orderList.Count == 0)
-            {
-                Destroy(customer.GetComponent<CustomerOrder>());
-            }
-        }
-    }
-
-    */
+    private List<string> orderList = new();
 
     private void OnEnable()
     {
